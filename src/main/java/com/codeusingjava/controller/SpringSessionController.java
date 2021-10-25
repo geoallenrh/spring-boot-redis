89162594b4c28e91d849aa7534b471068c2dce85
geoallenrh <geoffallen@redhat.com>
2021-10-25 17:49:49 -0500

    initi

---
+++ src/main/java/com/codeusingjava/controller/SpringSessionController.java
@@ -0,0 +1,50 @@
+package com.codeusingjava.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SpringSessionController {
+
+	@GetMapping("/")
+	public String home(Model redisModel, HttpSession redisSession) {
+		@SuppressWarnings("unchecked")
+		List<String> messages = (List<String>) redisSession.getAttribute("REDIS_SESSION_MESSAGES");
+
+		if (messages == null) {
+			messages = new ArrayList<>();
+		}
+		redisModel.addAttribute("sessionMessages", messages);
+		redisModel.addAttribute("sessionId", redisSession.getId());
+
+		return "index";
+	}
+
+	@PostMapping("/persistMessage")
+	public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest redisRequest) {
+		@SuppressWarnings("unchecked")
+		List<String> msgs = (List<String>) redisRequest.getSession().getAttribute("REDIS_SESSION_MESSAGES");
+		if (msgs == null) {
+			msgs = new ArrayList<>();
+			redisRequest.getSession().setAttribute("REDIS_SESSION_MESSAGES", msgs);
+		}
+		msgs.add(msg);
+		redisRequest.getSession().setAttribute("REDIS_SESSION_MESSAGES", msgs);
+		return "redirect:/";
+	}
+
+	@PostMapping("/destroy")
+	public String destroySession(HttpServletRequest redisRequest) {
+		redisRequest.getSession().invalidate();
+		return "redirect:/";
+	}
+}
